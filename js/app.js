0a63ee668378e792dfbda7640ce78bd38f350077
Gerson <gerson.x.alexandre@gmail.com>
2020-03-15 22:43:43 +0200

    creating a course object

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+
+//
+var name, duration, price, pre_work;
+
+function Course(name, duration, price, pre_work) {
+    this.name =name;
+    this.duration =duration;
+    this.price = price;
+    this.pre_work = pre_work
+}
+
+
+Course.prototype.describ =  function() {
+
+
+}
+
+let course = new Course(name, duration, price), pre_work;
+
+
+
