cd3290c7dcabc7ec7ee1496b5513412c7da9fb6d
Gerson <gerson.x.alexandre@gmail.com>
2020-04-03 11:09:35 +0200

    Creating UI, COntroller and Data Module. IMplementation of Hsopping cart hover effect on the navbar

--- js/app.js
+++ js/app.js
@@ -66,3 +66,115 @@ window.onload = function() {
 //========================================================================================================================
 //End of type writer
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Data Module
+var courseController = (function(){
+
+    //creating a function constructor
+    let Course = function(id, title, description, price, img){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.img = img;
+    };
+    
+    //creating a function prototype for shopping cart
+    let ShoppingCart = function(id, title, description, price, img, quantity){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.img = img;
+    };
+
+    //creating a data structure to put all data
+    let data = {
+
+        allItem:{
+            course: [],
+            shoppingCart: []
+        },
+
+        totalCourses: 0,
+        CartTotalItems: 0,
+        priceTotal: 0
+    }
+   
+    //create some initial courses to be displayed on the page
+    var course1 = new Course(1, 'Become a Full Stack web Developer', "12 Weeks Course - in Classroom", 26000,"none");
+    var course2 = new Course(2, 'Become a Front End Developer', '4 Weeks Course - in Classroom', 12000,"none");
+    var course3 = new Course(3, 'Become a Java Spring Boot Developer', "12 Weeks Course - in Classroom", 26000,"none");
+
+    //Push the initial courses to the course object
+    data.allItem.course.push(course1);
+    data.allItem.course.push(course2);
+    data.allItem.course.push(course3);
+   
+    return {
+      
+        //tesing function to log data in the console
+        testing: function(){
+            console.log(data);
+        },
+        
+        getData: function(){
+            return data;
+        }
+    }
+
+})();
+
+
+//*********************************************************************************************************************
+//UI Module
+var UIController = (function(){
+    //Store all the DOM strings 
+    let DOMString = {
+      
+    }
+
+    return {
+
+        getDOMString: function(){
+            return DOMString;
+        }
+    }
+
+})();
+
+
+
+//********************************************************************************************************************
+//Global App Controller
+var controller = (function (courseCtrl, UICtrl ){
+  
+    var cart = document.getElementById('cart');
+    let shoopingCart = document.getElementById('shop-cart');
+
+    
+    var setupEventListener = function(){
+ 
+        //show  shopping cart on mousehover
+        cart.addEventListener('mouseover', function(event){
+            event.preventDefault();
+            
+            if(shoopingCart.style.display === 'none'){
+                shoopingCart.style.display ='block';
+            } else {
+                shoopingCart.style.display ='none';
+            }          
+        });
+
+    }
+
+    return {
+        init: function(){
+            console.log("Application has started");
+            setupEventListener();
+        }
+    }
+
+})(courseController, UIController);
+
+controller.init();
